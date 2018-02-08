bf54c0fc2ff6c5653415e72ae691534783496eaa
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2018-02-08 16:29:45 -0500

    Ready for login queries!

--- lib/graphql/provider.dart
+++ lib/graphql/provider.dart
@@ -1,7 +1,22 @@
 import 'client.dart';
+import 'query.dart';
+import 'dart:async';
+
 
 const String baseApi = "http://api.limacitypass.com/gql";
 
 class Provider {
-    static final GQLClient client = new GQLClient(baseApi);
+    static final GQLClient client = new GQLClient(baseApi);    
+    static String _token = "";
+
+    set setToken(t) => _token = t;
+
+    static Future<GQLResponse> makeQuery(GQLQuery q) {
+        return client.run(q);
+    }
+
+    static Future<GQLResponse> makeAuthQuery(GQLQuery q) {
+        client.setJwtToken = _token;
+        return client.run(q, auth: true);
+    }
 }
